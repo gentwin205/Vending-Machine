9f91f27870f0338df71c024cf25c9e04b55badd9
Nathan Jamieson <sbgnjayy@gmail.com>
2023-06-08 15:52:40 -0400

    newchanges

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -9,6 +9,8 @@ import java.nio.file.FileSystemNotFoundException;
 import java.util.Scanner;
 
 public class VendingMachineCLI {
+	int counter = 5;
+	double currentMoney = 0.00;
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
@@ -19,9 +21,8 @@ public class VendingMachineCLI {
 	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
 	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
 
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT, MAIN_MENU_SECRET_OPTION };
-	private static final String[] PURCHASE_MENU_OPTIONS = { PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
-
+	private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT, MAIN_MENU_SECRET_OPTION};
+	private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
 
 
 	private Menu menu;
@@ -39,22 +40,28 @@ public class VendingMachineCLI {
 				// display vending machine items
 				String input = "vendingmachine.csv";
 				File vending = new File(input);
-				try(Scanner read = new Scanner(vending)){
-					while(read.hasNextLine()){
+				try (Scanner read = new Scanner(vending)) {
+					while (read.hasNextLine()) {
 						String line = read.nextLine();
-						System.out.println(line);
+						System.out.println(line + " " + counter);
 
 					}
-				}catch( Exception e ){
+				} catch (Exception e) {
 					System.out.println("file not found");
 
 				}
 
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
+				String choice2 = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+
 			}
+
+
 		}
 	}
+
+
 
 	public static void main(String[] args) {
 		Menu menu = new Menu(System.in, System.out);
