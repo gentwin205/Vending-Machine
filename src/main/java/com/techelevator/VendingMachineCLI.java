87e9d9eb29a159f12437072581dfd9b556792674
Jeremy Thompson <gentwin205@gmail.com>
2023-06-09 00:27:53 -0400

    menus workgit add -A!

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -4,15 +4,12 @@ import com.techelevator.view.ItemInventory;
 import com.techelevator.view.Items;
 import com.techelevator.view.Menu;
 
-import java.io.File;
-import java.lang.module.FindException;
-import java.nio.file.FileSystemNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 
 public class VendingMachineCLI {
-    int counter = 5;
     double currentMoney = 0.00;
 
     private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
@@ -32,6 +29,8 @@ public class VendingMachineCLI {
     private ItemInventory itemInventory;
 
 
+
+
     public VendingMachineCLI(Menu menu) {
         this.menu = menu;
     }
@@ -42,7 +41,7 @@ public class VendingMachineCLI {
 
             if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
                 // display vending machine items
-                this.itemInventory = new ItemInventory();
+                this.itemInventory = new ItemInventory();;
                 for (Map.Entry<String, Items> item : itemInventory.getVendingItems().entrySet()) {
                     System.out.println(item.getKey() + " " + item.getValue().getName() + " " + item.getValue().getPrice() + " " + item.getValue().getQuantity());
                 }
@@ -50,14 +49,31 @@ public class VendingMachineCLI {
 
             } else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
                 // do purchase
+                System.out.println("Current Money Provided: $" + currentMoney);
                 String choice2 = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+                Scanner scan = new Scanner(System.in);
+
 
                 if (choice2.equals(PURCHASE_MENU_OPTION_FEED_MONEY)) {
-                    System.out.println("FeedMoney");
+                    System.out.println("Please enter a dollar amount to add");
+                    double amount = scan.nextDouble();
+                    currentMoney += amount;
+
                 } else if (choice2.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
-                    System.out.println("Select Product");
+                    this.itemInventory = new ItemInventory();
+                    for (Map.Entry<String, Items> item : itemInventory.getVendingItems().entrySet()) {
+                        System.out.println(item.getKey() + " " + item.getValue().getName() + " " + item.getValue().getPrice() + " " + item.getValue().getQuantity());
+
+                        String slotItem = scan.nextLine();
+                        if (item.getKey().equals(slotItem)){
+                            System.out.println(slotItem);
+                        }
+                    }
+
                 } else if (choice2.equals(PURCHASE_MENU_OPTION_FINISH_TRANSACTION)) {
-                    System.out.println("Finish Transaction");
+                    double amountDue = currentMoney;
+                    currentMoney = 0.00;
+
 
                 }
 
