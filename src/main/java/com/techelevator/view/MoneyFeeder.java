44765196d943f6bc26e803c86544f04d9807eb3c
Nathan Jamieson <sbgnjayy@gmail.com>
2023-06-09 17:08:05 -0400

    this

--- src/main/java/com/techelevator/view/MoneyFeeder.java
+++ src/main/java/com/techelevator/view/MoneyFeeder.java
@@ -5,9 +5,18 @@ public class MoneyFeeder {
 
     private double totalMoney;
 
-    public MoneyFeeder(double moneyGiven, double totalMoney) {
+    private double returnMoney;
+
+    private double itemCost;
+
+    public double getReturnMoney() {
+        return returnMoney;
+    }
+
+    public MoneyFeeder(double moneyGiven, double totalMoney, double returnMoney) {
         this.moneyGiven = moneyGiven;
         this.totalMoney = totalMoney;
+        this.returnMoney = returnMoney;
     }
 
     public double getMoneyGiven() {
@@ -17,4 +26,13 @@ public class MoneyFeeder {
     public double getTotalMoney() {
         return totalMoney;
     }
+    public double change (double changeGiven){
+        totalMoney += moneyGiven;
+        itemCost -= totalMoney;
+        return changeGiven;
+
+
+
+    }
+
 }
